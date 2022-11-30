c84a358b9e22fd4055d08fe0ef645f045b5d8b0f
mohammadkhan <119054276+Mohammadkhan12345@users.noreply.github.com>
2022-11-30 08:00:04 -0800

    Add files via upload

---
+++ javascript/functions.js
@@ -0,0 +1,45 @@
+// var khan = "123";
+// console.log(khan)
+
+    // simple function
+
+// let x = 101;
+// let y = 202;
+
+//     function xyz()
+// {
+//     console.log("click");
+// }
+//     xyz ()
+
+//     let abc = xyz();
+
+// console.log(abc);
+
+
+    // @2 function with single argument
+
+// function show (your_name)
+// {
+//     console.log(your_name)
+// }    
+//     show("mohammadkhan"); 
+
+    // @3 function with double argument
+
+// function add (value1,value2)
+// {
+//   let sum = value1 + value2 ;
+//   console.log(sum)    
+//  } add(1,20)
+
+    // @4 function with double argument with return keyword
+
+// function sub (value1,value2)
+// {
+//    let minus = value1 - value2;
+//     return minus;
+// }
+
+// let result = sub (999,998);
+// console.log(result)
